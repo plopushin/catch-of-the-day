be59accda33a1e76fb42042825fb0dc16228edc7
plopushin <lopushin@ualberta.ca>
2018-03-23 13:40:27 -0700

    Add create fish, reset form

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,22 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+  state = {
+    fishes: {
+    },
+    order: {}
+  };
 
+  addFish = (fish) => {
+    //take a copy of the existing state
+    const fishes = {...this.state.fishes}; //take a copy of what is in state
+    // 2. Add our new fish to that fishes variable. 
+    fishes[`fish${Date.now()}`] = fish;
+    //3. Set the new fishes object to state
+    this.setState({
+      fishes: fishes
+    });
+  };
   render(){
     return(
       <div className="catch-of-the-day">
@@ -12,7 +27,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood" age={100} />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  
+  goToStore = event => {
+    //stop the form from submitting
+    event.preventDefault();
+    //2.get the text from input
+    const storeName = this.myInput.value.value;
+    //3. change page to store/x
+    this.props.history.push(`/store/${storeName}`);
+  };
+
   render(){
     return (
       
-     <form className="store-selector">
+     <form className="store-selector" onSubmit={this.goToStore}>
       <h2>Please enter a store</h2>
-      <input type="text" required placeholder ="Enter Store Name"/>
+      <input type="text" ref={this.myInput} required placeholder ="Enter Store Name" defaultValue={getFunName()} />
       <button type="submit">Visit Store</button>
     </form>
     
